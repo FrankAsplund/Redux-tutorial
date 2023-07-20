f0496594cc49cb4d2c80ba55e123b428e6a2a1a4
FrankAsplund <frank_asplund@hotmail.com>
2023-07-20 16:36:10 +0200

    Init commit

---
+++ components/LogIn.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="container px-4  items-center justify-between">
+          <div className="bg-[#1f2f6b] rounded-md mt-4 border-white sm:p-8 sm:mx-8 py-8">
+            <h2 className="flex justify-center text-3xl mt-4 mb-6 font-bold tracking-tight text-white sm:text-4xl">
+              Please log in
+            </h2>
+
+            <label>
+              <span className="my-2 block text-xl font-medium text-white-700">
+                Username
+              </span>
+            </label>
+            <input className="h-8 w-64 md:w-96 sm:p-2 text-black" />
+
+            <label>
+              <span className="mt-2 block text-xl font-medium text-white-700">
+                Password
+              </span>
+            </label>
+            <input className="h-8 w-64 md:w-96 sm:p-2 text-black" />
+
+            <div className="flex-col items-center mt-2">
+              <button
+                type="button"
+                className="w-24 mt-2 p-2 rounded-md border-white bg-[#0a1642]"
+              >
+                Log in
+              </button>
+              <button
+                type="button"
+                className="w-24 mx-2 p-2 rounded-md border-white bg-[#0a1642]"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
